15514d010b28190f211186fa5013d97528c293b0
hoangsrc <hoang.dm.cc25tth@ntu.edu.vn>
2026-08-01 22:13:36 +0700

    Hoàn thiện Lab 2 - Biến và Kiểu dữ liệu

--- BaiLab2_BienVaKieuDuLieu/src/Lab2.java
+++ BaiLab2_BienVaKieuDuLieu/src/Lab2.java
@@ -4,14 +4,14 @@ public class Lab2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// 1. Khai báo các biến
-        int a = 15;
-        int b = 4;
+        double a = 15;
+        double b = 4;
         double c = 3.5;
         
         // 2. Tính toán
-        int tong = a + b;
-        double thuong = a / c;
-        int phanDu = a % b;
+        double tong = a + b;
+        double thuong = a / b;
+        double phanDu = a % b;
         
         // 3. In kết quả
         System.out.println("Tổng: " + tong);
