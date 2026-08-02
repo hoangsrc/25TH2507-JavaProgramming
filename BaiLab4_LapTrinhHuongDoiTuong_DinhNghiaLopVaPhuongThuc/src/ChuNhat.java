14031f12a32dc39e2a9cb61959940d8821835bf4
hoangsrc <hoang.dm.cc25tth@ntu.edu.vn>
2026-08-02 17:02:08 +0700

    Khai báo thuộc tính và Hàm tạo

--- BaiLab4_LapTrinhHuongDoiTuong_DinhNghiaLopVaPhuongThuc/src/ChuNhat.java
+++ BaiLab4_LapTrinhHuongDoiTuong_DinhNghiaLopVaPhuongThuc/src/ChuNhat.java
@@ -1,4 +1,14 @@
 
 public class ChuNhat {
-
-}
+	//Khai báo các thuộc tính
+	private double dai;
+	private double rong;
+	//Hàm tạo
+	public ChuNhat() {
+		dai = 10; rong = 5;
+	}
+	public ChuNhat(double dai, double rong) {
+		this.dai = dai;
+		this.rong= rong;
+	}
+	
