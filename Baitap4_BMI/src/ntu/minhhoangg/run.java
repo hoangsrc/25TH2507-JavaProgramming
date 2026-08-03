a68c63727318ffa352420940b4bf34beb49cb114
hoangsrc <hoang.dm.cc25tth@ntu.edu.vn>
2026-08-03 21:06:28 +0700

    Bài 4 - chưa code

---
+++ Baitap4_BMI/src/ntu/minhhoangg/run.java
@@ -0,0 +1,14 @@
+package ntu.minhhoangg;
+
+import java.util.Scanner;
+
+public class run {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Khai báo Scanner
+		Scanner banPhim = new Scanner(System.in);
+		
+	}
+
+}
