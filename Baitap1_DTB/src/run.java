bd14b5080eec3c3ab1b097b79c07ce718f79e386
hoangsrc <hoang.dm.cc25tth@ntu.edu.vn>
2026-08-03 11:07:17 +0700

    Hoàn thiện Bài tập 1 - Nhập tên và điểm cho SV

--- Baitap1_DTB/src/run.java
+++ Baitap1_DTB/src/run.java
@@ -26,19 +26,22 @@ public class run {
 						}
 					} while (true);//while true làm cho vòng lặp cứ chạy cho tới khi gặp break;
 					
+				//Nhập điểm trung bình
 				System.out.print("Mời nhập điểm trung bình từ bàn phím: ");
-					DTB = banPhim.nextDouble();			
-					
-					
-					
-					
-					
-					
-					
-					
-					
-					
-					
+				do {
+				    if (banPhim.hasNextDouble()) {
+				        DTB = banPhim.nextDouble();
+				        if (DTB >= 0 && DTB <= 10) {
+				            break;
+				        } else {
+				            System.out.print("Lỗi: Điểm phải nằm trong khoảng từ 0 đến 10\nVui lòng nhập lại: ");
+				        }
+				    } else {
+				        // Nếu người dùng nhập chữ
+				        System.out.println("Lỗi: Điểm phải là một chữ số!");
+				        banPhim.next(); // Đọc bỏ giá trị chữ vừa nhập
+				    }
+				} while (true);
 				//In ra màn hình thông tin đã nhập từ bàn phím
 					System.out.println("Họ tên sinh viên: "+HoTen);
 					System.out.println("Điểm trung bình "+DTB);
